cf068d6920ef27e1821b62bc57bfacfd780e0c69
Duppps <ruanvasconcelos898@gmail.com>
2024-07-28 01:32:23 -0300

    selection sort implemented

---
+++ insertion_sort.hpp
@@ -0,0 +1,11 @@
+#ifndef INSERTION_SORT_HPP
+#define INSERTION_SORT_HPP
+
+#include <vector>
+
+class InsertionSort {
+public:
+    void sort(std::vector<int>& arr);
+};
+
+#endif
--- ordenation-algorithm.cpp
+++ ordenation-algorithm.cpp
@@ -1,9 +1,8 @@
-// ordenation-algorithm.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
-#include "bubble_sort.hpp"
-#include <vector>
 #include <iostream>
+#include <vector>
+#include "bubble_sort.hpp"
+#include "insertion_sort.hpp"
+#include "selection_sort.hpp"
 
 void printArray(const std::vector<int>& arr) {
     for (int num : arr) {
@@ -16,21 +15,23 @@ int main() {
     std::vector<int> arr = { 64, 34, 25, 12, 22, 11, 90 };
 
     BubbleSort bubbleSort;
-    bubbleSort.sort(arr);
+    InsertionSort insertionSort;
+    SelectionSort selectionSort;
 
-    std::cout << "Array Ordenado: ";
-    printArray(arr);
+    std::vector<int> arr1 = arr;
+    bubbleSort.sort(arr1);
+    std::cout << "Bubble Sorted array: ";
+    printArray(arr1);
+
+    std::vector<int> arr2 = arr;
+    insertionSort.sort(arr2);
+    std::cout << "Insertion Sorted array: ";
+    printArray(arr2);
+
+    std::vector<int> arr3 = arr;
+    selectionSort.sort(arr3);
+    std::cout << "Selection Sorted array: ";
+    printArray(arr3);
 
     return 0;
 }
-
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
-
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
